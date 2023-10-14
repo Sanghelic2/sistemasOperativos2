ec51d698691e1997078fb3663aff629df28ce69f
Alvaro Solano <a01643948@tec.mx>
2023-10-14 01:26:21 +0000

    practica 2

---
+++ practica2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    print ("soy el proceso padre\n");
+    int pid= fork();
+    if (pid == 0){
+        printf("soy el proceso hijo\n");
+    }else{
+        printf("soy el proceso padre y mi hijo es: %d\n",
+        pid);
+    }
+
+    return 0;
+}
